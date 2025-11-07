66dd253f522490bd04037b7d14d8df468e4d081e
Saksham <dubey.saksham@icloud.com>
2025-11-07 09:32:14 +0530

    initialValue variable declaration

--- lib/features/0_auth/screens/signup_screen.dart
+++ lib/features/0_auth/screens/signup_screen.dart
@@ -111,7 +111,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
 
                   // Role Dropdown
                   DropdownButtonFormField<String>(
-                    value: _selectedRole,
+                    initialValue: _selectedRole,
                     decoration: InputDecoration(
                       labelText: 'I am a...',
                       prefixIcon: const Icon(Icons.person_outline),
